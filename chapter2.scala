7a1c7a3f7b0d8b61d2fe5b4b9027037e156f1c65
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-06-20 13:14:15 +0900

    formatArgs

--- chapter2.scala
+++ chapter2.scala
@@ -1,4 +1,5 @@
 def printArgs(args: Array[String]): Unit = {
+  var i = 0
   while (i < args.length) {
     println(args(i))
     i += 1
@@ -10,3 +11,12 @@ def printArgsfun(args: Array[String]): Unit = {
     println(arg)
   }
 }
+
+def printArgsFunction(args: Array[String]): Unit = {
+  args.foreach(println)
+}
+
+def formatArgs(args: Array[String]) = args.mkString("\n")
+
+val args = Array("zero", "one", "two")
+println(formatArgs(args))
