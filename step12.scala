7cb1853b4175365864539789c7ca3f00b0671eeb
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-06-25 09:49:40 +0900

    change step12 read lines from a file code

--- step12.scala
+++ step12.scala
@@ -1,9 +1,8 @@
 import scala.io.Source
-val args = Array("chapter2.scala")
+
 if (args.length > 0) {
   for(line <- Source.fromFile(args(0)).getLines()) {
     println(line.length + " " + line)
   }
-} else {
-  Console.err.println("Please enter filename")
-}
+} else
+    Console.err.println("Please enter filename")
