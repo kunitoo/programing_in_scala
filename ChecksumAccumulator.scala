1afbd5a56863ac5af43ac98a85e87b3f2eb2984f
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-06-26 20:23:47 +0900

    change ChecksumAccumulator

--- ChecksumAccumulator.scala
+++ ChecksumAccumulator.scala
@@ -1,10 +1,6 @@
 class ChecksumAccumulator {
   private var sum = 0
-  def add(b: Byte): Unit = {
-    sum += b
-  }
-  def checksum(): Int = {
-    return ~(sum & 0xFF) + 1
-  }
+  def add(b: Byte): Unit = sum += b
+  def checksum(): Int = ~(sum & 0xFF) + 1
 }
 
