acb4490eeb0c67d43fcdc371237fc7e6dadb0314
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-07-19 13:02:31 +0900

    add summer and checksum

--- Summer.scala
+++ Summer.scala
@@ -1,9 +1,7 @@
 import ChecksumAccumulator.calculate
-object Summer {
-  def main(args: Array[String]) {
-    for(arg <- args) {
-      println(arg + ": " + calculate(arg))
-    }
+object Summer extends App {
+  for(arg <- args) {
+    println(arg + ": " + calculate(arg))
   }
 }
 
