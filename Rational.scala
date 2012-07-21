c8080be9967baca482b87c1afb6906ca14f173d1
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-07-21 19:54:19 +0900

    adding fields

--- Rational.scala
+++ Rational.scala
@@ -1,4 +1,11 @@
 class Rational(n: Int, d: Int) {
   require(d != 0)
+  val numer: Int = n
+  val denom: Int = d
   override def toString = n + "/" + d
+  def add(that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
 }
