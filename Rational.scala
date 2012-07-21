875f1ceb16937cd73137ea9253280abc8735a146
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-07-21 21:19:48 +0900

    Private fields and methods

--- Rational.scala
+++ Rational.scala
@@ -1,14 +1,17 @@
 class Rational(n: Int, d: Int) {
   require(d != 0)
-  val numer: Int = n
-  val denom: Int = d
+  private val g = gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
   def this(n: Int) = this(n, 1) // auxiliary constructor
-  override def toString = n + "/" + d
+  override def toString = numer + "/" + denom
   def add(that: Rational): Rational =
     new Rational(
       numer * that.denom + that.numer * denom,
       denom * that.denom
     )
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
   def lessThen(that: Rational) =
     this.numer * that.denom < that.numer * this.denom
   def max(that: Rational) =
