6b68c4b3083915b4af8e96dbc84a44012e8d4bf7
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-07-22 19:54:55 +0900

    Defining operators

--- Rational.scala
+++ Rational.scala
@@ -4,12 +4,14 @@ class Rational(n: Int, d: Int) {
   val numer: Int = n / g
   val denom: Int = d / g
   def this(n: Int) = this(n, 1) // auxiliary constructor
-  override def toString = numer + "/" + denom
-  def add(that: Rational): Rational =
+  def + (that: Rational): Rational =
     new Rational(
       numer * that.denom + that.numer * denom,
       denom * that.denom
     )
+  def * (that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+  override def toString = numer + "/" + denom
   private def gcd(a: Int, b: Int): Int =
     if (b == 0) a else gcd(b, a % b)
   def lessThen(that: Rational) =
